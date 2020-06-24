60a72004127b3333c023e40ef75c60114ddf275c
index-git <66255344+index-git@users.noreply.github.com>
2020-06-24 16:52:58 +0200

    First versions

---
+++ RandomWikipediaArticle/random_wikipedia_article.py
@@ -0,0 +1,18 @@
+import requests
+import webbrowser
+
+WikiURL = "https://cs.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json"
+ListOfYes = ["Y", "YES", "A", "ANO", ""]
+
+WikiRandomArticlesResp = requests.get(WikiURL)
+if WikiRandomArticlesResp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET '+WikiURL+' {}'.format(resp.status_code))
+
+
+for Article in WikiRandomArticlesResp.json()["query"]["random"]:
+    Response = input('Chcete otevřít článek {} {} [Yes/no]?'.format(Article['id'], Article['title'])) or "Yes"
+    if Response.upper() in ListOfYes:
+        ArticleURL = "https://cs.wikipedia.org/wiki?curid="+str(Article["id"])
+        webbrowser.open(ArticleURL)
+
