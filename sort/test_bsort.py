8c2384be1b390a9e3f669f3d5ba8b51797e700ad
index-git <66255344+index-git@users.noreply.github.com>
2020-06-29 16:32:13 +0200

    Few renames.

--- sort/test_bsort.py
+++ sort/test_bsort.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
-from Sort import sort
+from sort import bsort
 
 
 class TestBubbleSort(TestCase):
     def test_bubble_sort_small_num(self):
         nlist = list([2, 1])
-        assert sort.bubble_sort(nlist)["sorted_list"] == sorted(nlist)
+        assert bsort.bubble_sort(nlist)["sorted_list"] == sorted(nlist)
 
     def test_bubble_sort_big_num(self):
         nlist = list([58, 14, 92, 27, 62, 19, 7, 66])
-        assert sort.bubble_sort(nlist)["sorted_list"] == sorted(nlist)
+        assert bsort.bubble_sort(nlist)["sorted_list"] == sorted(nlist)
 
     def test_bubble_sort_small_string(self):
         nlist = list(["z", "a"])
-        assert sort.bubble_sort(nlist)["sorted_list"] == sorted(nlist)
+        assert bsort.bubble_sort(nlist)["sorted_list"] == sorted(nlist)
